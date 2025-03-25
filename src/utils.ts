7172edbb991a8129800610f4dfd285abed3a03fb
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2025-03-25 16:53:42 +0000

    Refactor utils.ts by removing unused functions for loading and sorting location keys

--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,6 @@
-import { Item, Metadata } from "@owlbear-rodeo/sdk";
-import { LocationKey } from "./@types/types";
-import { ID } from "./main";
 
-import Analytics from 'analytics';
 import googleAnalytics from '@analytics/google-analytics';
-
-export function loadExistingLocationKeys(
-  items: Item[],
-  newLocationKeys: LocationKey[],
-  getItemText: (item: any) => any
-) {
-  for (const item of items) {
-    if (item.metadata[`${ID}/metadata`]) {
-      const metadata = item.metadata[`${ID}/metadata`] as Metadata;
-      newLocationKeys.push({
-        description: metadata.locationKey as string,
-        name: getItemText(item),
-        id: item.id,
-      });
-    }
-  }
-}
-
-export function sortLocationKeys(newLocationKeys: LocationKey[]) {
-  newLocationKeys.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-  });
-}
+import Analytics from 'analytics';
 
 export const getItemText = (item: any) => {
   return item.text.richText[0].children[0].text ? item.text.richText[0].children[0].text : item.text.plainText;
