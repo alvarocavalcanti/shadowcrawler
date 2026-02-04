39a8c2ea19af61268d9fb69d10effd553d19bff9
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2026-02-04 13:28:03 +0000

    feat(ui): standardize donation buttons and add to What's New
    Create unified DonationButtons component with consistent h-9 sizing. Both buttons always appear together. Added to What's New popup to encourage support.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,6 +6,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faGlobeAfrica } from "@fortawesome/free-solid-svg-icons";
 import ThemeSelector from "./ThemeSelector";
 import { ThemeId } from "../themes";
+import DonationButtons from "./DonationButtons";
 
 const About: React.FC<{ version: string; currentTheme: ThemeId; onThemeChange: (theme: ThemeId) => void }> = ({ version, currentTheme, onThemeChange }) => {
   return (
@@ -47,25 +48,7 @@ const About: React.FC<{ version: string; currentTheme: ThemeId; onThemeChange: (
           <FontAwesomeIcon icon={faTwitter} /> alvarocavalcant
         </a>
       </div>
-      <div className="p-3 text-center">
-        <a href="https://www.buymeacoffee.com/alvarocavalcanti" target="_blank">
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-            alt="Buy Me A Coffee"
-            className="h-[60px] w-[217px] inline-block"
-          />
-        </a>
-      </div>
-      <div className="p-3 text-center">
-        <a href="https://ko-fi.com/O4O1WSP5B" target="_blank" rel="noreferrer">
-          <img
-            height="36"
-            className="h-9 inline-block"
-            src="https://storage.ko-fi.com/cdn/kofi6.png?v=6"
-            alt="Buy Me a Coffee at ko-fi.com"
-          />
-        </a>
-      </div>
+      <DonationButtons />
       <div className="text-center">
         <em className="text-gray-500 dark:text-gray-400 text-sm">Version: {version}</em>
       </div>
